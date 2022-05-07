5cb481f5ea7f780602293b075c155335e3759658
James Boby Vempala <jamesboby897@gmail.com>
2022-05-07 12:22:24 +0530

    Added basic calculator and arduino code
    Created a calculator that resembles stock android calculator and added basic arduino code.

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      blu: '#61b3ee',
+      grn:'#c5eed0',
+      lblu:'#d4e3fc',
+      gre:'#33383f',
+      dgre:'#1f1f1f',
+      crsr:'#99b2df',
+      wt:'#ffffff',
+      blk:'#000000',
+      mgre:'#282a2d',
+      pgrn:'#d1f1d9',
+      pblu:'#80c2f1',
+      plblu:'#dde9fc',
+      pgre1:'#4c4c4c',
+      pgre2:'#535557',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
